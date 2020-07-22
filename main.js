cca7844f9ad1a1663e581ec1b8ed9ed0a66fda1c
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2020-07-22 06:27:32 +0600

    initial commit

--- main.js
+++ main.js
@@ -1,67 +1,50 @@
-const lohinbtn = document.getElementById('login')
-lohinbtn.addEventListener('click', () => {
+const loginbtn = document.getElementById("login");
+loginbtn.addEventListener("click", () => {
+  const loginArea = document.getElementById("login-area");
+  loginArea.style.display = "none";
 
-const loginArea = document.getElementById('login-area')
-loginArea.style.display= 'none'
+  const transactionArea = document.getElementById("transaction-area");
 
-const transactionArea = document.getElementById('transaction-area')
-
-transactionArea.style.display= 'block'
-
-
+  transactionArea.style.display = "block";
 });
 
-const depositbtn = document.getElementById('deposit')
-depositbtn.addEventListener('click', () => {
-    // const depositamount = document.getElementById('depositamount').value
-    // const depositnumber = parseFloat(depositamount)
-    const depositnumber = getinputnumber('depositamount')
+const depositbtn = document.getElementById("deposit");
+depositbtn.addEventListener("click", () => {
+  const depositnumber =
+    getinputnumber("depositamount") 
+    updatespantext("currentDeposit", depositnumber);
 
-// const currentDeposit= document.getElementById('currentDeposit').innerText
+  updatespantext("currentbalance", depositnumber);
 
-// const currentDepositnumber = parseFloat(currentDeposit)
+  document.getElementById("depositamount").value = "";
+});
 
-// const totaldeposit =  depositnumber + currentDepositnumber 
-
-    
-
-// document.getElementById('currentDeposit').innerText = totaldepositc
-
-updatespantext('currentDeposit', depositnumber)
-
-updatespantext('currentbalance', depositnumber)
-
-document.getElementById('depositamount').value = ''
-
-
-})
-
-const withdrawbtn = document.getElementById('withdraw')
+const withdrawbtn = document.getElementById("withdraw");
 
 withdrawbtn.addEventListener("click", () => {
+  const withdrawnumber = getinputnumber("withdrawamount");
 
-    const withdrawnumber = getinputnumber('withdrawamount')
+  updatespantext("currentwithdraw", withdrawnumber);
+  updatespantext("currentbalance", -1 * withdrawnumber);
 
-console.log(withdrawnumber);
+  document.getElementById("withdrawamount").value = "";
+  console.log(withdrawnumber);
 
-    console.log('clicked');
-})
+  console.log("clicked");
+});
 
-function getinputnumber(id){
-    const amount = document.getElementById(id).value
-    const amountnumber = parseFloat(amount)
-    return amountnumber
+function getinputnumber(id) {
+  const amount = document.getElementById(id).value;
+  const amountnumber = parseFloat(amount);
+  return amountnumber;
 }
 
+function updatespantext(id, depositnumber) {
+  const current = document.getElementById(id).innerText;
 
-function updatespantext(id, depositnumber){
-    const current = document.getElementById(id).innerText
+  const currentnumber = parseFloat(current);
 
-    const currentnumber = parseFloat(current)
-    
-    const total = depositnumber + currentnumber
-    
-    document.getElementById(id).innerText = total  
-    
+  const total = depositnumber + currentnumber;
+
+  document.getElementById(id).innerText = total;
 }
-
